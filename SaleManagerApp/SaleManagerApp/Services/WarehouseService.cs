edf581a9cca57ee1e9c8c760703675a3efbe68d2
Ho Nhat Thanh <youareyou7.com@gmail.com>
2025-12-16 21:57:56 +0700

    Update WareHouse service

---
+++ SaleManagerApp/SaleManagerApp/Services/WarehouseService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaleManagerApp.Services
+{
+    public class ImportWarehouseService
+    {
+        private readonly DBConnectionService _db = new DBConnectionService();
+
+        public ImportIngredientResult ImportIngredient(ImportIngredientItem item)
+        {
+            try
+            {
+                using (var conn = _db.GetConnection())
+                using (var tran = conn.BeginTransaction())
+                {
+                    // 1. Tạo phiếu nhập
+                    var cmdImport = new SqlCommand(
+                        "sp_InsertImportOrder", conn, tran);
+
+                    cmdImport.CommandType = CommandType.StoredProcedure;
+                    cmdImport.Parameters.Add("@Note", SqlDbType.NVarChar, 100)
+                        .Value = item.note ?? "";
+
+                    string importOrderId = cmdImport.ExecuteScalar().ToString();
+
+                    // 2. Chi tiết nhập
+                    var cmdDetail = new SqlCommand(
+                        "sp_InsertImportOrderDetail", conn, tran);
+
+                    cmdDetail.CommandType = CommandType.StoredProcedure;
+                    cmdDetail.Parameters.Add("@ImportOrderId", SqlDbType.Char, 7)
+                        .Value = importOrderId;
+                    cmdDetail.Parameters.Add("@IngredientId", SqlDbType.Char, 7)
+                        .Value = item.ingredientId;
+                    cmdDetail.Parameters.Add("@Quantity", SqlDbType.Int)
+                        .Value = item.quantity;
+                    cmdDetail.Parameters.Add("@UnitPrice", SqlDbType.Money)
+                        .Value = 0; // kho không cần tiền
+
+                    cmdDetail.ExecuteNonQuery();
+
+                    tran.Commit();
+
+                    return new ImportIngredientResult
+                    {
+                        Success = true,
+                        SuccessMessage = "Nhập kho thành công"
+                    };
+                }
+            }
+            catch
+            {
+                return new ImportIngredientResult
+                {
+                    Success = false,
+                    ErrorMessage = "Lỗi nhập kho"
+                };
+            }
+        }
+    }
+}
+
---
+++ SaleManagerApp/SaleManagerApp/ViewModels/ImportWarehouseViewModel.cs
@@ -0,0 +1,62 @@
+﻿using SaleManagerApp.Helpers;
+using SaleManagerApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SaleManagerApp.ViewModels
+{
+    public class ImportWarehouseViewModel : BaseViewModel
+    {
+        private readonly ImportWarehouseService _service =
+            new ImportWarehouseService();
+
+        public string IngredientId { get; set; }
+        public int Quantity { get; set; }
+        public string Note { get; set; }
+
+        public ICommand ImportCommand { get; }
+        public ICommand CancelCommand { get; }
+
+        public Action CloseAction { get; set; }
+        public Action ReloadIngredient { get; set; }
+
+        public ImportWarehouseViewModel()
+        {
+            ImportCommand = new RelayCommand(Import);
+            CancelCommand = new RelayCommand(_ => CloseAction?.Invoke());
+        }
+
+        private void Import(object obj)
+        {
+            if (Quantity <= 0)
+            {
+                ToastService.ShowError("Số lượng không hợp lệ");
+                return;
+            }
+
+            var item = new ImportIngredientItem
+            {
+                ingredientId = IngredientId,
+                quantity = Quantity,
+                note = Note
+            };
+
+            var result = _service.ImportIngredient(item);
+
+            if (result.Success)
+            {
+                ReloadIngredient?.Invoke();
+                ToastService.Show(result.SuccessMessage);
+                CloseAction?.Invoke();
+            }
+            else
+            {
+                ToastService.ShowError(result.ErrorMessage);
+            }
+        }
+    }
+}
