b67324489544e17eeaf435fd90df52d102d23213
TuanLee <23521711@gm.uit.edu.vn>
2025-12-27 23:06:13 +0700

    Update UI

--- SaleManagerApp/Services/DBConnectionService.cs
+++ SaleManagerApp/Services/DBConnectionService.cs
@@ -11,7 +11,7 @@ namespace SaleManagerApp.Services
     public class DBConnectionService
     {
         private readonly string _connectString =
-            "Server=HP\\SQLEXPRESS;Database=SaleManagement20251_12;Trusted_Connection=True;";
+            "Server=TUANLEE\\SQLEXPRESS;Database=SaleManagement20251_12;Trusted_Connection=True;";
 
         public SqlConnection GetConnection()
         {
