29ac25e53bc42335ca3fd1e334a3bd8564943a8d
Ho Nhat Thanh <youareyou7.com@gmail.com>
2025-12-29 09:16:18 +0700

    Add IngredientModel

--- SaleManagerApp/SaleManagerApp/Models/IngredientItem.cs
+++ SaleManagerApp/SaleManagerApp/Models/IngredientItem.cs
@@ -6,67 +6,63 @@ using System.Runtime.CompilerServices;
 
 namespace SaleManagerApp.Models
 {
-    public enum IngredientGroup
-    {
-        Meat,       // Thịt
-        Seafood,    // Hải sản
-        Vegetable,  // Rau củ
-        Spice,      // Gia vị
-        Others
-    }
-
     public class IngredientItem : INotifyPropertyChanged
     {
-        // ===== MASTER DATA =====
+        // Master Data
         public string IngredientId { get; set; }
         public string IngredientName { get; set; }
+        public string Unit { get; set; }
+        public string Filter { get; set; }
+        public int MinQuantity { get; set; }
+        public string ImageUrl { get; set; }
+        public int MaxStorageDays { get; set; }
 
-        public string Unit { get; set; }              // kg, g, chai...
-        public IngredientGroup Group { get; set; }    // Nhóm nguyên liệu
-        public int MinQuantity { get; set; }          // Ngưỡng cảnh báo
+        // Batch History
+        public ObservableCollection<IngredientBatchHistory> Histories { get; }
+            = new ObservableCollection<IngredientBatchHistory>();
 
-        public string ImagePath { get; set; }         // Ảnh (local / url)
+        // Computed Properties
+        public int TotalQuantity =>
+            Histories
+                .Where(h => !h.IsDeleted && !h.IsExpired)
+                .Sum(h => h.RemainingQuantity);
 
-        // ===== BATCHES =====
-        public ObservableCollection<IngredientBatch> Batches { get; }
-            = new ObservableCollection<IngredientBatch>();
-
-        public IngredientItem()
-        {
-            Batches.CollectionChanged += (_, __) => RefreshStock();
-        }
-
-        // ===== COMPUTED PROPERTIES =====
-
-        // Tổng tồn kho
-        public int TotalQuantity => Batches.Sum(b => b.Quantity);
-
-        // Sắp hết hàng
         public bool IsLowStock => TotalQuantity <= MinQuantity;
 
-        // Có batch hết hạn
-        public bool HasExpiredBatch => Batches.Any(b => b.IsExpired);
+        public bool HasExpiredBatch =>
+            Histories.Any(h => h.IsExpired && !h.IsDeleted);
 
-        // Batch sắp hết hạn (trong 3 ngày)
         public bool HasNearExpiryBatch =>
-            Batches.Any(b => b.ExpiryDate.HasValue &&
-                             b.ExpiryDate.Value <= DateTime.Today.AddDays(3));
+            Histories.Any(h => !h.IsDeleted && h.IsNearExpiry);
 
+        public int ActiveBatchCount =>
+            Histories.Count(h => !h.IsDeleted && !h.IsExpired && h.RemainingQuantity > 0);
+
+        public IngredientBatchHistory NearestExpiryBatch =>
+            Histories
+                .Where(h => !h.IsDeleted && !h.IsExpired && h.RemainingQuantity > 0)
+                .OrderBy(h => h.ExpiryDate)
+                .FirstOrDefault();
+
+        public DateTime? EarliestExpiryDate => NearestExpiryBatch?.ExpiryDate;
+
+        // INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChanged(
-            [CallerMemberName] string name = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
-
-        // Gọi khi batch thay đổi
-        public void RefreshStock()
+        public void Refresh()
         {
             OnPropertyChanged(nameof(TotalQuantity));
             OnPropertyChanged(nameof(IsLowStock));
             OnPropertyChanged(nameof(HasExpiredBatch));
             OnPropertyChanged(nameof(HasNearExpiryBatch));
+            OnPropertyChanged(nameof(ActiveBatchCount));
+            OnPropertyChanged(nameof(NearestExpiryBatch));
+            OnPropertyChanged(nameof(EarliestExpiryDate));
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
---
+++ SaleManagerApp/SaleManagerApp/Models/WarehouseExport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SaleManagerApp.Models
+{
+    public class WarehouseExport
+    {
+        public string ExportId { get; set; }
+        public string IngredientId { get; set; }
+        public string HistoryId { get; set; }
+        public int QuantityExported { get; set; }
+        public DateTime ExportDate { get; set; }
+        public string EmployeeId { get; set; }
+        public string Note { get; set; }
+    }
+}
